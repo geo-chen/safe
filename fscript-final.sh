ac11c9ac2aa27d50d71bc5662aa455306be2acdb
SecuritySura <surangapremakumara@gmail.com>
2019-12-07 01:33:30 -0800

    updated
    added some features 
    
    lastb
    and fileupload location
    file name
    netstat command

--- fscript-final.sh
+++ fscript-final.sh
@@ -6,13 +6,12 @@
 # ssh linux1@172.16.44.156 sudo sh < /home/master/fscript-final.sh
 
 
-
 echo "create folder to save files \n "
 if [ -d "/tmp/fartifact" ]; then rm -Rf /tmp/fartifact; fi
 mkdir /tmp/fartifact
 
 echo "dumping password file \n"
-cat /etc/passwd | cut -d ":" -f1 | sort -u > /tmp/fartifact/uselist.log
+cat /etc/passwd | cut -d ":" -f1 | sort -u > /tmp/fartifact/userlist.log
 echo "done..!!! \n"
 
 echo "list all the root users \n"
@@ -21,6 +20,7 @@ echo "done..!!! \n"
 
 echo "dumping network statistic \n"
 # netstat -pant  | awk '{print $5}' | grep -v "0.0.0.0" | cut -d ":" -f1 | grep -v "and" | grep -v "Address" | sort -u > /tmp/fartifact/externalIPs.log
+
 netstat -panut | grep -v "udp6\|tcp6" | awk '{print $1, $4, $5, $6}'  | sort -u > /tmp/fartifact/externalIPs.log
 echo "done..!!! \n"
 
@@ -40,9 +40,10 @@ echo "dumping auto start services \n"
 ls -all /etc/init.d/ /lib/systemd/system/ /etc/systemd/system/ >> /tmp/fartifact/autostartservices.log
 echo "done..!!! \n"
 
-#echo "coping log files ....!!! \n"
-#find / -name "*.log" -exec cp {} /tmp/logs/ \;
-#echo "log dump done..!!! \n"
+echo "dumping bad logins"
+
+lastb > /tmp/fartifact/badlogins.log
+
 
 echo "coping log files ....!!! \n"
 cp /var/log/auth.log /tmp/fartifact/sshaccess.log
@@ -56,12 +57,16 @@ mkdir /tmp/fartifactupload
 echo "send files to master server \n"
 
 echo "add files to zip file"
-
+name=$(hostname)
+time=$(date +"%Y-%m-%d").zip
+echo $name
+echo $time
+filename=$name.$time
 #zip -r "/tmp/fartifactupload/archive-$(hostname)-$(date +"%Y-%m-%d").zip" /tmp/fartifact/
-zip -r "/tmp/fartifactupload/archive.zip" /tmp/fartifact/
+zip -r "/tmp/fartifactupload/$filename" /tmp/fartifact/
 
 echo '\n \n sending files to server \n'
 
-###sshpass -p '123' scp -r /tmp/fartifactupload/archive-ubuntu-2019-12-05.zip master@172.16.44.158:/home/master/forensic/evidence.zip
+###sshpass -p '123' scp -r /tmp/fartifactupload/archive-ubuntu-2019-12-05.zip master@172.20.10.4:/home/master/Dropbox/hacksmith/test/evidence.zip
 
-sshpass -p '123' scp -r /tmp/fartifactupload/archive.zip master@172.16.44.158:/home/master/Dropbox/evidence.zip
+sshpass -p '123' scp -r /tmp/fartifactupload/$filename master@172.20.10.4:/home/master/Dropbox/hacksmith/test/$filename
